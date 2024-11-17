1a1e02ea5c09a960cec4cf5bad82c3d7c11ff101
Poojitha <lellelapoojitha@gmail.com>
2024-11-17 21:29:08 +0530

    Sample maven Java

---
+++ src/main/java/com/app/demo/Samplemaven/App.java
@@ -0,0 +1,13 @@
+package com.app.demo.Samplemaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
